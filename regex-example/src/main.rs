b1ad6bb53c7728148fee580a793d9a05169a2697
cklellie <christopherjkelly@gmail.com>
2021-04-18 19:18:24 +0200

    Quick bit of unit testing.

--- regex-example/src/main.rs
+++ regex-example/src/main.rs
@@ -6,4 +6,15 @@ fn main() {
     println!("Did our date match? {}", re.is_match( "2021-04") );
 }
 
+fn check_match( re: Regex, v: &str )-> bool{
+    return re.is_match( v );
+}
+
+#[test]
+fn my_test(){
+    let re = Regex::new( r"^\d{4}-\d{2}-\d{2}$" ).unwrap();
+
+    assert!( check_match( re, "2021-04-12" ) );
+}
+
 
