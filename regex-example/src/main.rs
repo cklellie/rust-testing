ffb97688f7dbd5465e53770fcce719a7646fa3ef
cklellie <christopherjkelly@gmail.com>
2021-04-18 19:40:22 +0200

    Virtual workspace for root.

--- regex-example/src/main.rs
+++ regex-example/src/main.rs
@@ -2,11 +2,11 @@ use regex::Regex;
 
 fn main() {
     let re = Regex::new( r"^\d{4}-\d{2}-\d{2}$" ).unwrap();
-    println!("Did our date match? {}", re.is_match( "2021-04-18") );
-    println!("Did our date match? {}", re.is_match( "2021-04") );
+    println!("Did our date match? {}", check_match( &re, "2021-04-18") );
+    println!("Did our date match? {}", check_match( &re,"2021-04") );
 }
 
-fn check_match( re: Regex, v: &str )-> bool{
+fn check_match( re: &Regex, v: &str )-> bool{
     return re.is_match( v );
 }
 
@@ -14,7 +14,7 @@ fn check_match( re: Regex, v: &str )-> bool{
 fn my_test(){
     let re = Regex::new( r"^\d{4}-\d{2}-\d{2}$" ).unwrap();
 
-    assert!( check_match( re, "2021-04-12" ) );
+    assert!( check_match( &re, "2021-04-12" ) );
 }
 
 
